4282ae99f6f4858d887fc51f975e7160000f0d2e
Garret Gallant <gallanttechnological@gmail.com>
2022-07-19 10:06:58 -0600

    lesson complete

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React from "react"
+const bio = true
 
-function About() {
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {bio ? <p>{props.bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <a href={props.github}>{props.github}</a>
+      <a href={props.linkedin}>{props.linkedin}</a>
     </div>
   );
 }
