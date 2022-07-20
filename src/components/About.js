461c56ce97e7d36bfb1d2b23b9e6b4456d6983f4
Garret Gallant <gallanttechnological@gmail.com>
2022-07-19 19:31:18 -0600

    commit tree 4961d3b794f7a65c32069cafed4e773ffbdf4560

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      {bio ? <p>{props.bio}</p> : null}
+      {props.bio ? <p>{props.bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       <a href={props.github}>{props.github}</a>
       <a href={props.linkedin}>{props.linkedin}</a>
